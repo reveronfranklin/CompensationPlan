77fb361231e95e473dea52c1055cba78331f1f91
mmhgarcia <mmhgarcia@gmail.com>
2019-09-24 22:19:32 -0400

    Helper, PCSysFile, NavMenu

--- Compensaction.Share/PCSysfile.cs
+++ Compensaction.Share/PCSysfile.cs
@@ -23,10 +23,10 @@ namespace Compensaction.Share
 
         public decimal UmbralOrdenesPignoradas{ get; set; }
                
-<<<<<<< HEAD
+//<<<<<<< HEAD
   
-=======
->>>>>>> 17fa1a4fda41ff04cb0d68a5d6966eee2cc13074
+//=======
+//>>>>>>> 17fa1a4fda41ff04cb0d68a5d6966eee2cc13074
 
         public int DiasPagoDoble { get; set; }
 
